69649416201e3968257b96c3dab0969c3d4dd5ab
Ahmad Natfaji <anatfaji@snelstart.nl>
2023-02-13 14:23:59 +0100

    header restyle

--- src/Angular-UI/src/app/components/header/header.component.ts
+++ src/Angular-UI/src/app/components/header/header.component.ts
@@ -11,42 +11,34 @@ export class HeaderComponent implements OnInit {
 	constructor() { }
 
 	ngOnInit(): void {
+		var elements = document.querySelectorAll(".menu-nav-link");
+		var max = 0;
 
-		// document.querySelector(document).addEventListener("click", "li.dropdown-control > a", function (event) {
-		// 	event.preventDefault()
-		// 	fieldName = document.querySelector(this).attr("data-dropdown");
-		// 	if (document.querySelector("ul[name=" + fieldName + "]").classList.contains("show")) {
-		// 		document.querySelector("ul[name=" + fieldName + "]").classList.remove("show");
-		// 		document.querySelector("i[name=" + fieldName + "]").classList.remove("fa-chevron-up").classList.add("fa-chevron-down");
-		// 	} else {
-		// 		if (document.querySelector("ul.dd-menu.show").length >= 1) {
-		// 			document.querySelector("ul.dd-menu.show").classList.remove("show");
-		// 			document.querySelector("i#arrow").classList.remove("fa-chevron-up").classList.add("fa-chevron-down");
-		// 		}
-		// 		document.querySelector("ul[name=" + fieldName + "]").classList.add("show");
-		// 		document.querySelector("i[name=" + fieldName + "]").classList.remove("fa-chevron-down").classList.add("fa-chevron-up");
-		// 	}
-		// });
+		elements.forEach(element => {
+			var width = element.clientWidth;
+			if (width > max) max = width;
+		});
 
+		elements.forEach(element => {
+			this.setWidth(element, max);
+		});
+	}
+
+	private setWidth(element: any, w: number) {
+		element.style.width = w + 'px';
 	}
 
 	toggleMenu() {
 		if (!this.menuOpen) {
-			document.querySelectorAll(".menuburger, nav, .menu-nav, .menu-nav-item")?.forEach(element => {
-				element.classList.add("open");
+			document.querySelectorAll(".menu-nav")?.forEach(element => {
+				element.classList.add("expanded");
 			});
 
 			this.menuOpen = true
 		} else {
-			document.querySelectorAll(".menuburger, nav, .menu-nav, .menu-nav-item")?.forEach(element => {
-				element.classList.remove("open");
+			document.querySelectorAll(".menu-nav")?.forEach(element => {
+				element.classList.remove("expanded");
 			});
-
-			if (document.querySelector("ul.dd-menu")?.classList.contains("show")) {
-				document.querySelector("ul.dd-menu")?.classList.remove("show");
-				document.querySelector("i#arrow")?.classList.remove("fa-chevron-up");
-				document.querySelector("i#arrow")?.classList.add("fa-chevron-down");
-			}
 
 			this.menuOpen = false
 		}

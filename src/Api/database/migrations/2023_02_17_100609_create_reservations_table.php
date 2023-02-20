496d6d78a5ef34c3f39ef90a890fa311a3c4f7e3
AstralPlays <milanbruul@gmail.com>
2023-02-20 11:01:49 +0100

    Edit migrations

--- src/Api/database/migrations/2023_02_17_100609_create_reservations_table.php
+++ src/Api/database/migrations/2023_02_17_100609_create_reservations_table.php
@@ -15,8 +15,11 @@ return new class extends Migration
     {
         Schema::create('reservations', function (Blueprint $table) {
             $table->id();
-            $table->string('email');
-            $table->string('password');
+            $table->string('userID');
+            $table->string('verified');
+            $table->date('date');
+            $table->time('start_time');
+            $table->time('end_time');
             $table->timestamp('created_at')->useCurrent();
             $table->timestamp('updated_at')->useCurrent();
             $table->softDeletes();

083d8a64bf03f89b676b398e502915c9c30fa252
AstralPlays <milanbruul@gmail.com>
2023-02-20 14:15:26 +0100

    Register Module Made

--- src/Api/app/Modules/AccountSystem/Http/Controllers/AccountSystemController.php
+++ src/Api/app/Modules/AccountSystem/Http/Controllers/AccountSystemController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Modules\AccountSystem\Clients\Contracts\AccountSystemClientInterface;
 use App\Modules\AccountSystem\Requests\StoreRegisterRequest;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 class AccountSystemController extends Controller
 {
@@ -21,14 +22,16 @@ class AccountSystemController extends Controller
 
     public function register(StoreRegisterRequest $request)
     {
+        $uuid = Str::uuid()->toString();
         $account = $this->userLoginClient->create(
             [
                 'email' => $request['email'],
                 'password' => Hash::make($request),
+                'api_token' => $uuid
             ]);
         return [
             'id' => $account['id'],
-            'api_token' => 'test1234'
+            'api_token' => $uuid
         ];
     }
 

79b6d782fad20bc6027b2cd33e0db240d42f1fad
AstralPlays <milanbruul@gmail.com>
2023-02-20 09:41:43 +0100

    Finished Push

--- src/Api/routes/api.php
+++ src/Api/routes/api.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/all', [ReservationController::class, 'index'])->name('all');
+Route::get('/reservation/all', [ReservationController::class, 'index'])->name('all');
 
 Route::get('/users/all', [UserLoginController::class, 'index'])->name('allUsers');
 

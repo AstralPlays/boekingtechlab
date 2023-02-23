9d5cfbdfa2851fe56dc61fa370a71df5e99b628a
Ahmad Natfaji <modemoon2003@hotmail.com>
2023-02-23 23:32:38 +0100

    router to reservation

--- src/Angular-UI/src/app/app-routing.module.ts
+++ src/Angular-UI/src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginPageComponent } from './components/login-page/login-page.component';
 import { RegisterPageComponent } from './components/register-page/register-page.component';
+import { ReservationPageComponent } from './components/reservation-page/reservation-page.component';
 import { Test401Component } from './components/test401/test401.component';
 import { UserComponent } from './components/user/user.component';
 import { AuthGuard } from './guards/auth.guard';
 
 const routes: Routes = [
+	{ path: 'reservation', title: "Boeking tech lab | reservation", component: ReservationPageComponent },
 	{ path: 'login', title: "Boeking Tech Lab | Login", component: LoginPageComponent },
 	{ path: 'register', title: "Boeking Tech Lab | Register", component: RegisterPageComponent },
 	{ path: 'unauthorized', title: "Boeking Tech Lab | unauthorized", component: Test401Component },

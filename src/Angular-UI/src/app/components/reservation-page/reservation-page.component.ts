90f20dec9ac5167d9c903d931dd87356f312698a
Ahmad Natfaji <modemoon2003@hotmail.com>
2023-02-23 17:01:40 +0100

    reservation page component

---
+++ src/Angular-UI/src/app/components/reservation-page/reservation-page.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { FormArray, FormBuilder, FormControl, FormGroup } from '@angular/forms';
+
+@Component({
+	selector: 'app-reservation-page',
+	templateUrl: './reservation-page.component.html',
+	styleUrls: ['./reservation-page.component.scss']
+})
+export class ReservationPageComponent implements OnInit {
+
+	constructor() { }
+
+	ngOnInit(): void {
+
+	}
+
+}

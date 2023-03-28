36881aecd4a7481cb9d2aca9f6ad3590888bdc8a
Ahmad Natfaji <modemoon2003@hotmail.com>
2023-03-28 14:21:14 +0200

    layout

---
+++ src/Api/resources/views/layouts/default.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Boekingtechlab</title>
+    <link href="https://fonts.googleapis.com/css2?family=Exo+2:wght@100;200;300;400;500;600;700;800;900&display=swap"
+        rel="stylesheet">
+    @vite(['resources/scss/app.scss', 'resources/js/app.js'])
+</head>
+
+<body>
+    <x-background />
+    <header>
+        <x-header />
+    </header>
+
+    <main>
+        @yield('content')
+    </main>
+
+    <footer>
+    </footer>
+</body>
+
+</html>

fac2c6661124ecab140360eeaf3c06dec8fa6644
Ahmad Natfaji <modemoon2003@hotmail.com>
2023-06-19 11:40:36 +0200

    config

--- src/Api/vite.config.js
+++ src/Api/vite.config.js
@@ -5,10 +5,10 @@ export default defineConfig({
 	plugins: [
 		laravel({
 			input: [
-				'resources/scss/app.scss',
 				'resources/scss/about-us.scss',
 				'resources/scss/admin-dashboard.scss',
 				'resources/scss/admin-reservations.scss',
+				'resources/scss/app.scss',
 				'resources/scss/background.scss',
 				'resources/scss/header.scss',
 				'resources/scss/home-page.scss',
@@ -16,6 +16,9 @@ export default defineConfig({
 				'resources/scss/register.scss',
 				'resources/scss/reservation.scss',
 				'resources/scss/side-bar.scss',
+				'resources/scss/user-dashboard.scss',
+				'resources/scss/user-reservations.scss',
+				'resources/scss/user-settings.scss',
 				'resources/js/app.js',
 				'resources/js/reservation.js',
 			],
